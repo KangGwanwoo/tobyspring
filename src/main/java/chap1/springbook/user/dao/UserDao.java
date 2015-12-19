f7475ebd5b42b3991079e2b2c3db8d3137075f70
kanggwanwoo <rkdrhksdn2@gmail.com>
2015-12-19 17:27:25 +0900

    중복 코드의 메소드 추출
    커넥션을 가졍는 중복된 코드를 분리.
    관심의 종류에 따라 코드를 구분해놓으면, 한 가지 관심에 대한 변경이 일어날 경우 그 관심이 집중되는 부분의 코드만 수정하면 된다.
    여기서 사용한 getConnection() 메소드처럼 중복된 코드를 뽑아내는 것을 리팩토링에서는 메소드 추출(extract method)기법이라고 부른다.

--- src/main/java/chap1/springbook/user/dao/UserDao.java
+++ src/main/java/chap1/springbook/user/dao/UserDao.java
@@ -9,10 +9,7 @@ import java.sql.*;
  */
 public class UserDao {
     public void add(User user) throws ClassNotFoundException,SQLException{
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/tobySpring","root","Starter?5"
-        );
+        Connection c = getConnection();
 
         PreparedStatement ps = c.prepareStatement(
                 "insert into users(id, name, password) values(?,?,?)"
@@ -21,7 +18,6 @@ public class UserDao {
         ps.setString(1, user.getId());
         ps.setString(2, user.getName());
         ps.setString(3, user.getPassword());
-
         ps.executeUpdate();
 
         ps.close();
@@ -29,10 +25,10 @@ public class UserDao {
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection c = DriverManager.getConnection(
-                "jdbc:mysql://localhost/tobySpring","root","Starter?5"
-        );
+        Connection c = getConnection();
+
+
+
         PreparedStatement ps = c.prepareStatement(
                 "select * from users where id = ?"
         );
@@ -49,6 +45,13 @@ public class UserDao {
         c.close();
 
         return user;
+    }
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection(
+                "jdbc:mysql://localhost/tobySpring", "root", "Starter?5"
+        );
     }
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
