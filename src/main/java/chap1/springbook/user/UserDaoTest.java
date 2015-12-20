a18d877531d37be7c082162cc5ab2ec2d6491129
kanggwanwoo <rkdrhksdn2@gmail.com>
2015-12-20 20:29:24 +0900

    XML을 이용하는 애플리케이션 컨텍스트 134p
    자바 코드를 이용하면 변경이 있을 때 마다 일일히 컴파일해야 하는 수고가 있다. XML을 이용해서 빈 설정 정보를 저장하고 애플리케이션 컨텍스트가 참조하면 이러한 수고를 덜 수 있다.

--- src/main/java/chap1/springbook/user/UserDaoTest.java
+++ src/main/java/chap1/springbook/user/UserDaoTest.java
@@ -7,6 +7,7 @@ import chap1.springbook.user.dao.UserDao;
 import chap1.springbook.user.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -16,7 +17,7 @@ import java.sql.SQLException;
 public class UserDaoTest {
 
     public static void main(String[] args) throws ClassNotFoundException,SQLException{
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("spring-config.xml");
         UserDao dao = context.getBean("userDao",UserDao.class);
         User user = new User();
         user.setId("whiteship");
