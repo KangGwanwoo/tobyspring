1f35cc1bfddfb7fba516a18387aff95432b7920e
kanggwanwoo <rkdrhksdn2@gmail.com>
2015-12-27 15:56:51 +0900

    Junit Test로 전환
    main()메소드를 이용한 테스트 작성 방법만으로는 애플리케이션 규모가 커지고 테스트 개수가 많아지면 테스트를 수행하는 일이 점점 부담이 될 것이다.
    main 메소드는 또한 제어권을 사용자가 직접 가져가기 때문에 프레임워크를 사용한다는 개념이 적용되기 힘들다.
    새로 만들 테스트 메소드는 Junit 프레임워크가 요구하는 조건 두가지를 따라야 한다.
    첫째는 메소드가 public으로 선언돼야 하는 것이고, 다른 하나는 메소드에 @Test라는 애노테이션을 붙여주는 것이다.

--- src/test/java/foo/bar/UserDaoTest.java
+++ src/test/java/foo/bar/UserDaoTest.java
@@ -1,38 +1,40 @@
-package chap1.springbook.user;
+package foo.bar;
 
 import chap1.springbook.user.dao.ConnectionMaker;
 import chap1.springbook.user.dao.DConnectionMaker;
 import chap1.springbook.user.dao.DaoFactory;
 import chap1.springbook.user.dao.UserDao;
 import chap1.springbook.user.domain.User;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
+import static org.hamcrest.CoreMatchers.is;
+
 /**
  * Created by daum on 15. 12. 20..
  */
 public class UserDaoTest {
 
-    public static void main(String[] args) throws ClassNotFoundException,SQLException{
+    @Test
+    public void addAndGet() throws ClassNotFoundException,SQLException{
         GenericXmlApplicationContext context = new GenericXmlApplicationContext("spring-config.xml");
         UserDao dao = context.getBean("userDao",UserDao.class);
         User user = new User();
-        user.setId("whiteship");
-        user.setName("백기선");
-        user.setPassword("married");
+        user.setId("gyumee");
+        user.setName("박상철");
+        user.setPassword("springno1");
 
         dao.add(user);
 
-        System.out.println(user.getId() + "등록 성공");
-
         User user2 = dao.get(user.getId());
-        System.out.println(user2.getName());
 
-        System.out.println(user2.getPassword());
+        Assert.assertThat(user2.getName(),is(user.getName()));
+        Assert.assertThat(user2.getPassword(),is(user.getPassword()));
 
-        System.out.println(user2.getId() + "조회 성공");
     }
 }
