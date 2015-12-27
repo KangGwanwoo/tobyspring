d3070d1ca5e01593505e5f1544e07b14289fba39
kanggwanwoo <rkdrhksdn2@gmail.com>
2015-12-27 16:21:12 +0900

    DAO에 deleteAll()과 getCount() 추가
    일관성 있는 결과를 보장하는 테스트를 만들기 위해 DAO에 추가 메소드를 만들고, 테스트에서 사전 환경 설정을 해준다.
    count() 테스트를 통해서 getCount() 메소드가 확실히 동작하는지에 대해서 꼼꼼한 테스트를 진행한다.
    addAndGet 테스트는 각 id값에 대해서 올바른 유저를 가져오는지에 대해서도 테스트한다.

--- src/test/java/foo/bar/UserDaoTest.java
+++ src/test/java/foo/bar/UserDaoTest.java
@@ -21,20 +21,47 @@ import static org.hamcrest.CoreMatchers.is;
 public class UserDaoTest {
 
     @Test
-    public void addAndGet() throws ClassNotFoundException,SQLException{
+    public void addAndGet() throws ClassNotFoundException, SQLException {
         GenericXmlApplicationContext context = new GenericXmlApplicationContext("spring-config.xml");
-        UserDao dao = context.getBean("userDao",UserDao.class);
-        User user = new User();
-        user.setId("gyumee");
-        user.setName("박상철");
-        user.setPassword("springno1");
 
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user1 = new User("gyumee", "박상철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+
+        dao.deleteAll();
+        Assert.assertThat(dao.getCount(), is(0));
+
+
+        dao.add(user1);
+        dao.add(user2);
+        Assert.assertThat(dao.getCount(), is(2));
+
+        User userget1 = dao.get(user1.getId());
+        Assert.assertThat(userget1.getName(), is(user1.getName()));
+        Assert.assertThat(userget1.getPassword(), is(user1.getPassword()));
+
+        User userget2 = dao.get(user2.getId());
+        Assert.assertThat(userget2.getName(), is(user2.getName()));
+        Assert.assertThat(userget2.getPassword(), is(user2.getPassword()));
+    }
+
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        GenericXmlApplicationContext context = new GenericXmlApplicationContext("spring-config.xml");
+        UserDao dao = context.getBean("userDao", UserDao.class);
+        User user = new User("gyumee", "박상철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+        User user3 = new User("bumjin", "박범진", "springno3");
+
+
+        dao.deleteAll();
+        Assert.assertThat(dao.getCount(), is(0));
         dao.add(user);
-
-        User user2 = dao.get(user.getId());
-
-        Assert.assertThat(user2.getName(),is(user.getName()));
-        Assert.assertThat(user2.getPassword(),is(user.getPassword()));
+        Assert.assertThat(dao.getCount(), is(1));
+        dao.add(user2);
+        Assert.assertThat(dao.getCount(), is(2));
+        dao.add(user3);
+        Assert.assertThat(dao.getCount(), is(3));
 
     }
 }
