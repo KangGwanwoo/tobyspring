cf5ec73daaaa7fd7a562dad1879152e05c2119e8
KangGwanwoo <rkdrhksdn2@gmail.com>
2016-01-01 15:43:36 +0900

    익명 클래스로 빼는 이유 - 227p
    지금까지 만든 코드는 DAO 메소드마다 전략 클래스를 생성해야하고, 부가 정보가 있을 때 마다 추가적인 처리가 필요했다.
    이는 따로 클래스 파일을 만들어서 생생하는 것이 아니라, 익명 클래스를 context에 전달해줌으로써 해결할 수 있었다.

--- src/main/java/chap1/springbook/user/dao/UserDao.java
+++ src/main/java/chap1/springbook/user/dao/UserDao.java
@@ -18,9 +18,17 @@ public class UserDao {
 
     public UserDao(){
     }
-    public void add(User user) throws ClassNotFoundException,SQLException{
-        StatementStrategy st = new AddStatement(user);
-        jdbcContextStatementStrategy(st);
+    public void add(final User user) throws ClassNotFoundException,SQLException{
+        jdbcContextStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?,?,?)");
+                ps.setString(1, user.getId());
+                ps.setString(2, user.getName());
+                ps.setString(3, user.getPassword());
+                return ps;
+            }
+        });
     }
 
     public User get(String id) throws ClassNotFoundException, SQLException {
@@ -50,8 +58,13 @@ public class UserDao {
     }
 
     public void deleteAll() throws SQLException{
-        StatementStrategy st = new DeleteAllStatement();
-        jdbcContextStatementStrategy(st);
+        jdbcContextStatementStrategy(new StatementStrategy() {
+            @Override
+            public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
+                PreparedStatement ps = c.prepareStatement("delete from users");
+                return ps;
+            }
+        });
     }
 
     public void jdbcContextStatementStrategy(StatementStrategy stmt) throws SQLException{
