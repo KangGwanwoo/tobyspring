fae67aeae6744ed176191087b3054ffebffabf82
kd4 <kangddanddan@gmail.com>
2016-02-28 19:01:15 +0900

    프록시 팩토리 빈 방식의 장.단점 - 461p
    다이내믹 프록시를 이용하면 타깃 인터페이스를 구현하는 클래스를 일일이 만드는 번거로움을 제거할 수 있다.
    하나의 핸들러 메솓를 구현하는 것만으로도 수많은 메소드에 부가기능을 부여해줄 수 있으니 부가기능 코드의 중복 문제도 사라지고, DI 통해서 번거로운 생성 코드도 제거할 수 있다. 하지만 코드 수정 없이 설정의 변경만으로 수천 개 이상의 메소드에 새로운 기능을 추가할 수 있다는 점은 분명 대단한 일이지만, 이 방식은 설정파일을 통해서 구현하기 때문에 설정파일이 급격히 복잡해진다. 바람직하지 못하다.

---
+++ src/main/java/chap1/springbook/user/service/TxProxyFactoryBean.java
@@ -0,0 +1,51 @@
+package chap1.springbook.user.service;
+
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by kd4 on 2016. 2. 28..
+ */
+public class TxProxyFactoryBean implements FactoryBean<Object> {
+    Object target;
+    PlatformTransactionManager transactionManager;
+    String pattern;
+    Class<?> serviceInterface;
+
+    public void setServiceInterface(Class<?> serviceInterface) {
+        this.serviceInterface = serviceInterface;
+    }
+
+    public void setPattern(String pattern) {
+        this.pattern = pattern;
+    }
+
+    public void setTarget(Object target) {
+        this.target = target;
+    }
+
+    public void setTransactionManager(PlatformTransactionManager transactionManager) {
+        this.transactionManager = transactionManager;
+    }
+
+    @Override
+    public Object getObject() throws Exception {
+        TransactionHandler txHandler = new TransactionHandler();
+        txHandler.setTarget(target);
+        txHandler.setTransactionManager(transactionManager);
+        txHandler.setPattern(pattern);
+        return Proxy.newProxyInstance(getClass().getClassLoader(),new Class[] {serviceInterface},txHandler);
+    }
+
+    @Override
+    public Class<?> getObjectType() {
+        return serviceInterface;
+    }
+
+    @Override
+    public boolean isSingleton() {
+        return false;
+    }
+}
